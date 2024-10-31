1a3cbd12a58b12d0354b721ee14858d6caf3ab76
bensatlantik <bensantora@proton.me>
2024-10-31 09:01:33 -0400

    Fix README.md conflict markers

---
+++ target/package/rich_err-0.1.0/target/debug/deps/rich_err-cb669709e8d49761.d
@@ -0,0 +1,5 @@
+/home/ben/rich_err/target/debug/deps/rich_err-cb669709e8d49761: src/lib.rs
+
+/home/ben/rich_err/target/debug/deps/rich_err-cb669709e8d49761.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/package/rich_err-0.1.0/target/debug/librich_err.d
@@ -0,0 +1,1 @@
+/home/ben/rich_err/target/package/rich_err-0.1.0/target/debug/librich_err.rlib: /home/ben/rich_err/target/package/rich_err-0.1.0/src/lib.rs
